fc725ba337ffaecf3d28fb69da891c7ec23bcdbb
Rafael Lopes <freitasr@amazon.com>
2018-09-08 12:45:07 -0400

    Implementing a better way to update the IP address

--- ip-reporter/ipupdater.sh
+++ ip-reporter/ipupdater.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-
-sn=$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2)
+apikey=$(cat apikey.txt)
+[ -f /proc/cpuinfo ] && sn=$(cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2) || sn="none"
 snr="${sn: -5}"
 wifiip=$(ip addr | grep inet | grep wlan0 | awk -F" " '{print $2}'| sed -e 's/\/.*$//')
 [ -f $BASEDIR/wifiip.txt ] && previousip=$(cat $BASEDIR/wifiip.txt) || previousip=""
@@ -12,7 +12,7 @@ date
 if [ "$wifiip" != "$previousip" ]
   then
     echo "New wifi IP addr detected: $wifiip"
-    /home/pi/.local/bin/aws route53 change-resource-record-sets --hosted-zone-id ZVLG22V7GRDZ3 --change-batch "{\"Comment\":\"Wifi Address change\",\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$snr.simplecoffeeservice.cloud.\",\"Type\":\"A\",\"TTL\":30,\"ResourceRecords\":[{\"Value\":\"$wifiip\"}]}}]}"
+    curl -X PUT -H "X-API-Key: $apikey" https://jn7b4963df.execute-api.us-east-1.amazonaws.com/prod/upsert?ip=$wifiip&name=$snr
     echo $wifiip > $BASEDIR/wifiip.txt
   else
     echo "IP addr not modified"
